9a65fa6e96a106efdd673f4b152e9a691b98a633
Michela Marchini <mmarchini@stuy.edu>
2018-03-26 00:26:12 -0400

    updated

--- bars.js
+++ bars.js
@@ -1,18 +1,39 @@
-var data = [2.99, 3.54, 16.5] //2016
-var data2 = [2.55, 2.82, 14.5] //2006
-var chart = d3.select(".chart");
-var bar = chart.selectAll("div");
-var barUpdate = bar.data(data);
-var barEnter = barUpdate.enter().append("div");
+var list_2016 = [2.99, 3.54, 16.5]; //2016
+var list_2006 = [2.55, 2.82, 14.5]; //2006
 
-var button = document.getElementById("switch");
 
-var transition = function(e){
+var button_2016 = document.getElementById("2016");
+var button_2006 = document.getElementById("2006");
+
+
+var changeData = function(e){
+    data_2016 = this.getAttribute("id") == "2016";
+    console.log(data_2016);
+
+    data = [];
+
+    if (data_2016){
+	data = list_2016;
+    }
+    else{
+	data = list_2006;
+    }
+
+    console.log(data);
+    
+    d3.select(".chart").selectAll("div").remove();
+
+    var bar  = d3.select(".chart").selectAll("div");
+    var barUpdate = bar.data(data);
+    var barEnter = barUpdate.enter().append("div");
+
+    barEnter.transition()
+	.duration(2000)
+	.style("width",function(d){return d*20 + "px";})
+	.text(function(d){return d;})   
 }
 
-var changeData = function(){
-}
+//barEnter.text(function(d) { return d; });
 
-barEnter.text(function(d) { return d; });
-
-button.addEventListener("click", changeData);
+button_2006.addEventListener("click", changeData);
+button_2016.addEventListener("click", changeData);
